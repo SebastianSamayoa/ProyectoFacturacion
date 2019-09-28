b4b3384222ba0d6ed851adb5a8526f26feb8f2ff
SebastianSamayoa <samayoa_jhoan@hotmail.com>
2019-09-28 09:39:36 -0600

    Creacion del Repositorio

---
+++ Facturas/src/main/java/com/proyecto/Facturas/FacturasApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.Facturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturasApplication.class, args);
+	}
+
+}
