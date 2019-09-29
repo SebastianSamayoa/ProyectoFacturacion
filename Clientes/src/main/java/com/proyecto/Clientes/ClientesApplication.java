5fa1d6988b8d7de760a10805dd750b883a0730a8
SebastianSamayoa <samayoa_jhoan@hotmail.com>
2019-09-29 11:59:22 -0600

    Agregando la informacion requerida

--- Clientes/src/main/java/com/proyecto/Clientes/ClientesApplication.java
+++ Clientes/src/main/java/com/proyecto/Clientes/ClientesApplication.java
@@ -1,13 +1,14 @@
 package com.proyecto.Clientes;
 
+import com.proyecto.Clientes.Implementacion.ImplCliente;
+import com.proyecto.Clientes.Objeto.Cliente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientesApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(ClientesApplication.class, args);}
+
 
 }
